d6f79465833dc669cbdea7e07ac0d7507fb3b921
gonziremylo-svg <gonziremylo@gmail.com>
2026-03-04 11:30:45 -0300

    stock.js

--- stock.js
+++ stock.js
@@ -1,2 +1,32 @@
+
 const stock = [
-];
+
+{articulo:"DARLON GLITTER CORAZON BEIGE", bacacay:74.00, deposito:918.90},
+
+{articulo:"DARLON GLITTER CORAZON CELESTE", bacacay:67.40, deposito:356.80},
+
+{articulo:"DARLON GLITTER CORAZON GRIS", bacacay:75.30, deposito:417.20},
+
+{articulo:"DARLON GLITTER CORAZON LILA", bacacay:76.90, deposito:918.90},
+
+{articulo:"DARLON GLITTER CORAZON ROSA", bacacay:82.10, deposito:920.90},
+
+{articulo:"DARLON GLITTER ESTRELLA BEIGE", bacacay:77.50, deposito:101.00},
+
+{articulo:"DARLON GLITTER ESTRELLA CELESTE", bacacay:72.00, deposito:748.20},
+
+{articulo:"DARLON GLITTER ESTRELLA GRIS", bacacay:66.00, deposito:734.50},
+
+{articulo:"DARLON GLITTER ESTRELLA ROSA", bacacay:71.10, deposito:304.80},
+
+{articulo:"DARLON LISO BEIGE", bacacay:172.60, deposito:1246.10},
+
+{articulo:"DARLON LISO BORDO", bacacay:108.00, deposito:1221.80},
+
+{articulo:"DARLON LISO CELESTE", bacacay:162.60, deposito:1456.40},
+
+{articulo:"DARLON LISO CHOCOLATE", bacacay:95.30, deposito:608.40},
+
+{articulo:"DARLON LISO MARINO", bacacay:138.20, deposito:717.20}
+
+]
