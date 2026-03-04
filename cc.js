3b9c401b2a531f2643508149690b39f3fc0c68ee
gonziremylo-svg <gonziremylo@gmail.com>
2026-03-04 11:09:11 -0300

    cc.js

---
+++ cc.js
@@ -0,0 +1,31 @@
+const cuentas = [
+
+{
+cliente:"PEDRO Y CELIA LOCAL",
+ars:10825770,
+usd:0,
+movimientos:[]
+},
+
+{
+cliente:"MARIANA PACHECO",
+ars:929700,
+usd:0,
+movimientos:[]
+},
+
+{
+cliente:"MARCELO GUSMAN",
+ars:45366490,
+usd:0,
+movimientos:[]
+},
+
+{
+cliente:"ROGER ZITANA",
+ars:0,
+usd:11599,
+movimientos:[]
+}
+
+];
